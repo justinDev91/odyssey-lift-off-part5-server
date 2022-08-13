65a88783e9c285cedd99dac9163c03bd9e42de01
justinDev91 <justin@sustainsoft.eu>
2022-08-13 11:09:15 +0200

    Heroku needs our server to run on a port specified by the PORT environment variable, which is set behind the scenes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ async function startApolloServer(typeDefs, resolvers) {
       };
     },
   });
-
-  const { url, port } = await server.listen();
+ //Heroku needs our server to run on a port specified by the PORT environment variable, which is set behind the scenes.
+ //Apollo Server is listening on port 4000
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
